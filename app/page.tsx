551d36eb4c9599e5ffe577caf502124562e98c92
DraconDev <dracsharp@gmail.com>
2023-12-11 19:16:09 +0000

    basic example

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,37 @@
+"use client";
+import { useAtom } from "jotai";
+import { citiesAtom, countAtom, countryAtom } from "../state/atoms";
+
+// import countAtom
+
 export default function Home() {
-    return <main></main>;
+    const [count, setCount] = useAtom(countAtom);
+    const [country, setCountry] = useAtom(countryAtom);
+    const [cities, _] = useAtom(citiesAtom);
+    return (
+        <main className="flex min-h-screen flex-col items-center gap-2  bg-slate-700">
+            <div className="gap-2 flex">
+                <button onClick={() => setCount(count + 1)}>Increment</button>
+                {count}
+            </div>
+            <div className="gap-2 flex">
+                <button onClick={() => setCount(count - 1)}>Decrement</button>
+                {count}
+            </div>
+
+            <div className="">{country}</div>
+
+            <div className="flex justify-center flex-col items-center">
+                <div className="p-2">Cities</div>
+                {cities.map((city, i) => (
+                    <p
+                        className=""
+                        key={i}
+                    >
+                        {city}
+                    </p>
+                ))}
+            </div>
+        </main>
+    );
 }
--- state/atoms.ts
+++ state/atoms.ts
@@ -1,9 +1,9 @@
 import { atom } from "jotai";
 
-const countAtom = atom(0);
-const countryAtom = atom("Japan");
-const citiesAtom = atom(["Tokyo", "Kyoto", "Osaka"]);
-const mangaAtom = atom({
+export const countAtom = atom(0);
+export const countryAtom = atom("Japan");
+export const citiesAtom = atom(["Tokyo", "Kyoto", "Osaka"]);
+export const mangaAtom = atom({
     "Dragon Ball": 1984,
     "One Piece": 1997,
     Naruto: 1999,
